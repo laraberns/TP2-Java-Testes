dd2a8e8ed9c211ce1356bd7ced33cf174608fb0e
Lara Pereira <larabernspereira@gmail.com>
2025-05-14 20:54:40 -0300

    exer 4

--- src/main/java/br/edu/infnet/CalculadoraReembolso.java
+++ src/main/java/br/edu/infnet/CalculadoraReembolso.java
@@ -1,7 +1,7 @@
 package br.edu.infnet;
 
 public class CalculadoraReembolso {
-    public double calcularValorReembolsado(double valorFixo, double percentualReembolso){
+    public double calcularValorReembolsado(double valorFixo, double percentualReembolso, Paciente paciente){
         if(valorFixo < 0 || percentualReembolso < 0 || percentualReembolso > 100){
             throw new IllegalArgumentException("Valores inválidos");
         }
--- src/test/java/br/edu/infnet/CalculadoraReembolsoTest.java
+++ src/test/java/br/edu/infnet/CalculadoraReembolsoTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraReembolsoTest {
     private CalculadoraReembolso calculadoraReembolso;
+    private Paciente pacienteDummy;
 
     @BeforeEach
     void setUp() {
@@ -16,32 +17,32 @@ class CalculadoraReembolsoTest {
     // Exercício 1
     @Test
     void deveCalcularValorReembolsado() {
-        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(200, 70);
+        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(200, 70, pacienteDummy);
         assertEquals(140, valorReembolsado);
     }
 
     // Exercício 2
     @Test
     void deveRetornarValorTotalQuandoPercentualFor100() {
-        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(200, 100);
+        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(200, 100, pacienteDummy);
         assertEquals(200, valorReembolsado);
     }
 
     @Test
     void deveRetornarZeroQuandoPercentualForZero() {
-        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(200, 0);
+        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(200, 0, pacienteDummy);
         assertEquals(0, valorReembolsado);
     }
 
     @Test
     void deveRetornarZeroQuandoValorConsultaForZero() {
-        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(0, 100);
+        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(0, 100, pacienteDummy);
         assertEquals(0, valorReembolsado);
     }
 
     @Test
     void deveRetornar100QuandoValorEPercentualForem100() {
-        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(100, 100);
+        double valorReembolsado = calculadoraReembolso.calcularValorReembolsado(100, 100, pacienteDummy);
         assertEquals(100, valorReembolsado);
     }
 }
